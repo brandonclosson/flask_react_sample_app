8755c1a8c04e18f46fd0931ce0a015d4b35ebe96
brandonclosson <bclosson@gmail.com>
2018-04-16 23:22:39 -0500

    map users using functional component

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,18 +1,22 @@
 import axios from 'axios'
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import UsersList from './components/UsersList'
 
 
 class App extends Component {
     constructor() {
         super();
+        this.state = {
+            users: []
+        };
     };
-    componentDidMount {
+    componentDidMount() {
         this.getUsers();
     };
     getUsers() {
         axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-            .then((res) => { console.log(res); })
+            .then((res) => { this.setState({ users: res.data.data.users }) })
             .catch((err) => { console.log(err); });
     };
     render() {
@@ -23,6 +27,7 @@ class App extends Component {
                         <br />
                         <h1>All Users</h1>
                         <hr/><br/>
+                        <UsersList users={this.state.users} />
                     </div>
                 </div>
             </div>
